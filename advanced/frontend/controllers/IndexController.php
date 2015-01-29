3935f7864653c653995cbec7d2e12370e88bdc4c
123jian <1048124167@qq.com>
2015-01-29 10:42:40 +0800

    a

--- advanced/frontend/controllers/IndexController.php
+++ advanced/frontend/controllers/IndexController.php
@@ -8,5 +8,37 @@ class IndexController extends \yii\web\Controller
     {
         return $this->renderpartial('index');
     }
+	public function actionLogin()
+    {
+        return $this->renderpartial('login');
+    }
+	public function actionMall()
+    {
+        return $this->renderpartial('mall');
+    }
+	public function actionPayment()
+    {
+        return $this->renderpartial('payment');
+    }
+	public function actionPaymentok()
+    {
+        return $this->renderpartial('paymentok');
+    }
+	public function actionProtype()
+    {
+        return $this->renderpartial('protype');
+    }
+	public function actionRegister()
+    {
+        return $this->renderpartial('register');
+    }
+	public function actionShopping()
+    {
+        return $this->renderpartial('shopping');
+    }
+	public function actionShoppingmsg()
+    {
+        return $this->renderpartial('shoppingmsg');
+    }
 
 }
